e2dfc1b35c66005200922bfddb9d8176612fa7e7
Adam Backstrom <adam@sixohthree.com>
2013-07-25 17:55:56 -0400

    Remove GPL code from WordPress 3.3.2

--- functions.php
+++ functions.php
@@ -1,19 +1,9 @@
 <?php
 
-// http://core.trac.wordpress.org/browser/tags/3.3.2/wp-includes/general-template.php
-function selected( $selected, $current = true, $echo = true ) {
-	return __checked_selected_helper( $selected, $current, $echo, 'selected' );
-}
+function selected($selected, $current) {
+    if ($selected === $current) {
+        return 'selected';
+    }
 
-// http://core.trac.wordpress.org/browser/tags/3.3.2/wp-includes/general-template.php
-function __checked_selected_helper( $helper, $current, $echo, $type ) {
-	if ( (string) $helper === (string) $current )
-		$result = " $type='$type'";
-	else
-		$result = '';
-
-	if ( $echo )
-		echo $result;
-
-	return $result;
+    return '';
 }
