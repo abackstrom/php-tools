0dcfa0b8da22c21945d303535255667724bd05de
Adam Backstrom <adam@sixohthree.com>
2013-07-25 23:22:59 -0400

    Add GitHub fork ribbon

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ if( 'POST' === $_SERVER['REQUEST_METHOD'] ) {
 </section>
 
 <footer>
-    <a href="https://github.com/abackstrom/php-tools">php-tools</a> by <a href="https://github.com/abackstrom">abackstrom</a>.
+    <a href="https://github.com/abackstrom/php-tools"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
 </footer>
 
 </body>
